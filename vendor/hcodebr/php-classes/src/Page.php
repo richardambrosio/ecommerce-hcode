08cea3edd368144c032d12b0a577b722dc7bb014
Richard Ambrosio <richard.ambrosio1@live.com>
2022-01-19 15:39:25 -0300

    Admin - login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,6 +9,8 @@ class Page {
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header" => true,
+        "footer" => true,
         "data" => []
     ];
 
@@ -28,7 +30,7 @@ class Page {
 
         $this->setData($this->options['data']);
 
-        $this->tpl->draw('header');
+        if ($this->options['header']) $this->tpl->draw('header');
     }
 
     public function setTpl($name, $data = array(), $returnHTML = false) {
@@ -44,7 +46,7 @@ class Page {
     }
 
     public function __destruct() {
-        $this->tpl->draw('footer');
+        if ($this->options['footer']) $this->tpl->draw('footer');
     }
 
 }
